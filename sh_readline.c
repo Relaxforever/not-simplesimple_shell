8ca70182b5427fc09bf2bf80827246628fc56d23
root <869@holbertonschool.com>
2019-08-28 17:24:18 +0000

    Uploading the Readline funct

---
+++ sh_readline.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+* sh_readline - get a line from the user
+* Return: returns the text of the line readed
+*/
+
+char *sh_readline(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+
+	if (getline(&line, &len, stdin) == -1)
+	{
+		perror("not found\n");
+		free(line);
+		exit(0);
+	}
+return (line);
+}
